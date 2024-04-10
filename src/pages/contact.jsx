dee610f7cd3506857d163732f2495b647dbe964f
3ple-C <triplec871@gmail.com>
2024-04-10 13:23:28 +0100

    Updated slider component and contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -26,7 +26,7 @@ const Contact = () => {
             <Navbar />
             <Header1 src={HeaderImg} src2={HeaderImg2} />
 
-            <section className='flex xl:px-28    lg:pt-32 lg:px-10 '  >
+            <section className='flex xl:px-28 lg:pt-32 lg:px-10 '  >
                 <div className='w-[50%] flex flex-col gap-12' id='contacts'>
                     <Contacts source={EmailIcon} name="Email Address" contacts="Info@theresurrectedchurchng.org" />
                     <Contacts source={Whatsapp} name="Whatsapp" contacts="09139107522" />
@@ -37,7 +37,7 @@ const Contact = () => {
                 <div className='lg:relative xl:w-[60%] md:w-[75%] md:mt-[1.85rem] px-4 pt-5 mx-auto' >
                     <div id='border-control'></div>
 
-                    <div className="bg-white form-container space-y-7 lg:space-y-10 md:space-y-7 lg:shadow-lg" id='contact' >
+                    <div className="bg-white form-container space-y-7 xl:px-7 xl:py-2 xl:rounded-md lg:space-y-10 md:space-y-7 lg:shadow-lg" id='contact' >
                         <FormHeader header="Send us a message" subHeader="Can’t find what you’re looking for? Need help? Feel free by sending us a message." className='pt-0' />
 
                         <div className='md:w-[50%] flex flex-col gap-12' id='contacts2'>
